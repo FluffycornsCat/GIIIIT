e8b7a458d9458cbdeffa5aecc66bb4719ffa4d7f
Cilsakius <ceciliadbeck@gmail.com>
2019-08-19 11:05:51 +0200

    I DID IT

--- digt.py
+++ digt.py
@@ -1,4 +1,4 @@
 print("""Pakken ser og ophæver\nen frihed og denne lætte\ngård bliver silende
-et træ fra sovende kvinder
 stivnet i deres forskels
+et træ fra sovende kvinder
 gård på kage hoved findes ud.""")
