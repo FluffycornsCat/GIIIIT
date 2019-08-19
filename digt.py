0317f72e7c595215f7523ae60365ca9b6c3c0e65
FluffycornsCat <39726440+FluffycornsCat@users.noreply.github.com>
2019-08-19 10:57:01 +0200

    nytnytnyt

--- digt.py
+++ digt.py
@@ -1,7 +1,4 @@
-"Pakken ser og ophæver
-en frihed og denne lætte
-gård bliver silende
+print("""Pakken ser og ophæver\nen frihed og denne lætte\ngård bliver silende
 et træ fra sovende kvinder
 stivnet i deres forskels
-gård på kage hoved findes ud."
-asdfgbnmcdfghgf
+gård på kage hoved findes ud.""")
