dc1b62e97fb542e4863cdc8ff01bdad369639b51
FluffycornsCat <39726440+FluffycornsCat@users.noreply.github.com>
2019-08-19 11:07:28 +0200

    slettede en linje

--- digt.py
+++ digt.py
@@ -1,4 +1,4 @@
-print("""Pakken ser og ophæver\nen frihed og denne lætte\ngård bliver silende
+print("""Pakken ser og ophæver\ngård bliver silende
 stivnet i deres forskels
 et træ fra sovende kvinder
 gård på kage hoved findes ud.""")
