b05d94ef9f257294882d7a5298af3f1221c6f279
FluffycornsCat <39726440+FluffycornsCat@users.noreply.github.com>
2019-08-19 10:47:43 +0200

    jeg har slettet en linje yo

--- digt.py
+++ digt.py
@@ -1,1 +1,1 @@
-print("Pakken ser og ophæver\nen frihed og denne tunge\nvej bliver silende\net træ fra vågnende kvinder\nstivnet i deres forskels\ngård på noget hoved findes ud.")
+print("Pakken ser og ophæver\nen frihed og denne tunge\net træ fra vågnende kvinder\nstivnet i deres forskels\ngård på noget hoved findes ud.")
