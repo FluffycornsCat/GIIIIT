bc4a95721b64b0a859fa1e2a4ba67384099b942b
Dysted <37104912+Dysted@users.noreply.github.com>
2019-08-19 10:49:36 +0200

    linje 2 linje 3 linje 4 linje 6

--- digt.py
+++ digt.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 "Pakken ser og ophæver
 en frihed og denne lætte
 gård bliver silende
@@ -5,3 +6,6 @@ et træ fra sovende kvinder
 stivnet i deres forskels
 gård på kage hoved findes ud."
 asdfgbnmcdfghgf
+=======
+print("Pakken ser og ophæver\nen frihed og denne tunge\nvej bliver silende\net træ fra vågnende kvinder\nstivnet i deres forskels\ngård på noget hoved findes ud.")
+>>>>>>> 1f5971d9f83eebcef9e5d2541f8049dd7b46f5e0
